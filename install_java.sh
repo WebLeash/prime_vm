bbebdf6df48f23fee7170f430384ca3d651f9883
Nathan Stott <nathan.stott@sky.com>
2021-03-04 18:13:26 +0000

    adding java jenkins

---
+++ install_java.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install default-jre
+java -version
+sudo apt install default-jdk
+javac -version
+
---
+++ install_jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installing Jenkins"
+
+
+echo "Long Term Support release"
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
